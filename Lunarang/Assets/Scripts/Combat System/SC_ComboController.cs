15833a29b69078e93d2d080c41b0cb90a48d8fb6
shuufleurr@gmail.com <shuufleurr@gmail.com>
2023-10-09 16:18:13 +0200

    Animation Controller

--- Lunarang/Assets/Scripts/Combat System/SC_ComboController.cs
+++ Lunarang/Assets/Scripts/Combat System/SC_ComboController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 public class SC_ComboController : MonoBehaviour
@@ -12,27 +14,54 @@ public class SC_ComboController : MonoBehaviour
     
     [Header("Debug")]
     public int comboCounter = 0;
-    public WeaponType weaponType = WeaponType.WeaponA;
+    public WeaponType weaponType = WeaponType.Null;
 
     public List<WeaponType> currentComboWeaponTypes = new List<WeaponType>();
-
     
+    public WeaponType nextType = WeaponType.Null;
+    private bool canPerformCombo;
+    
+    private Animator _animator;
     
     #endregion
 
     #region Init
-    
+
+    private void Awake()
+    {
+        if (!TryGetComponent(out _animator)) return;
+    }
+
     // Start is called before the first frame update
     private void Start()
     {
-        SC_InputManager.instance.weaponA.performed += _ => IncrementCombo(WeaponType.WeaponA);
-        SC_InputManager.instance.weaponB.performed += _ => IncrementCombo(WeaponType.WeaponB);
-        SC_InputManager.instance.weaponC.performed += _ => IncrementCombo(WeaponType.WeaponC);
+        AttachInputToAttack();
+    }
+    
+    private void AttachInputToAttack()
+    {
+        SC_InputManager.instance.weaponA.performed += _ => Attack(WeaponType.WeaponA);
+        SC_InputManager.instance.weaponB.performed += _ => Attack(WeaponType.WeaponB);
+        SC_InputManager.instance.weaponC.performed += _ => Attack(WeaponType.WeaponC);
     }
 
     #endregion
 
     #region Functions
+
+    private void Attack(WeaponType type)
+    {
+        
+        IncrementCombo(type);
+        UpdateAnimator();
+        
+    }
+
+    private void UpdateAnimator()
+    {
+        _animator.SetInteger("Combo", comboCounter);
+        _animator.SetInteger("Type", (int)weaponType);
+    }
     
     /// <summary>
     /// Check if the current combo reach its max length.
@@ -68,6 +97,33 @@ public class SC_ComboController : MonoBehaviour
         currentComboWeaponTypes.Clear();
     }
 
+    // private void AttachInputBuffering()
+    // {
+    //     SC_InputManager.instance.weaponA.performed += _ => InputBuffering(WeaponType.WeaponA);
+    //     SC_InputManager.instance.weaponB.performed += _ => InputBuffering(WeaponType.WeaponB);
+    //     SC_InputManager.instance.weaponC.performed += _ => InputBuffering(WeaponType.WeaponC);
+    // }
+    //
+    // private void DetachInputBuffering()
+    // {
+    //     SC_InputManager.instance.weaponA.performed -= _ => InputBuffering(WeaponType.WeaponA);
+    //     SC_InputManager.instance.weaponB.performed -= _ => InputBuffering(WeaponType.WeaponB);
+    //     SC_InputManager.instance.weaponC.performed -= _ => InputBuffering(WeaponType.WeaponC);
+    // }
+    //
+    // private void InputBuffering(WeaponType type)
+    // {
+    //     if (nextType == WeaponType.Null)
+    //     {
+    //         nextType = type;
+    //     }
+    // }
+    //
+    // private void LaunchInputBuffered()
+    // {
+    //     Attack(nextType);
+    // }
+    
     #endregion
     
 }
@@ -75,6 +131,7 @@ public class SC_ComboController : MonoBehaviour
 public enum WeaponType
 {
     
+    Null,
     WeaponA,
     WeaponB,
     WeaponC
