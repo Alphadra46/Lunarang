0b419cd066ccb87f8871c014fc4f5a00c35b60e1
Shuu <shuufleurr@gmail.com>
2023-10-11 12:28:10 +0200

    Gravityyyyyyyyyyy POWAAAAA

--- Lunarang/Assets/Scripts/AI/SC_AIStats.cs
+++ Lunarang/Assets/Scripts/AI/SC_AIStats.cs
@@ -9,20 +9,20 @@ public class SC_AIStats : MonoBehaviour
     #region Variables
     
     [Header("Stats Settings")]
-    [Tooltip("Current base HP of the enemy")] public float hpBase = 0;
-    [Tooltip("Current HP multiplier of the enemy")] public float hpMultiplier = 0;
+    [Tooltip("Current base HP of the enemy")] public float hpBase = 5;
+    [Tooltip("Current HP multiplier of the enemy")] public float hpMultiplier = 1;
     [Tooltip("Current effective HP of the enemy")] private float hpEffective;
     
     [Tooltip("Current base DEF of the enemy")] public float defBase = 0;
-    [Tooltip("Current DEF multiplier of the enemy")] public float defMultiplier = 0;
+    [Tooltip("Current DEF multiplier of the enemy")] public float defMultiplier = 1;
     [Tooltip("Current effective DEF of the enemy")] private float defEffective;
     
     [Tooltip("Current base ATK of the enemy")] public float atkBase = 0;
-    [Tooltip("Current ATK multiplier of the enemy")] public float atkMultiplier = 0;
+    [Tooltip("Current ATK multiplier of the enemy")] public float atkMultiplier = 1;
     [Tooltip("Current effective ATK of the enemy")] private float atkEffective;
     
     [Tooltip("Current base Speed of the enemy")] public float speedBase = 0;
-    [Tooltip("Current Speed multiplier of the enemy")] public float speedMultiplier = 0;
+    [Tooltip("Current Speed multiplier of the enemy")] public float speedMultiplier = 1;
     [Tooltip("Current effective Speed of the enemy")] private float speedEffective;
 
     [Header("Status")] public List<WeaponType> currentWeakness;
