fb171fdfe890699b79fbb5f50bf63b1500843efd
Shuu <56737334+ShuuFleur@users.noreply.github.com>
2023-12-07 13:45:48 +0100

    Updated Animator Controller

--- Lunarang/Assets/Scripts/StateMachine/AttackAnimatorState.cs
+++ Lunarang/Assets/Scripts/StateMachine/AttackAnimatorState.cs
@@ -7,7 +7,7 @@ public class AttackAnimatorState : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.GetComponent<SC_PlayerController>().FreezeMovement(true);
+        animator.transform.parent.GetComponent<SC_PlayerController>().FreezeMovement(true);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -19,7 +19,7 @@ public class AttackAnimatorState : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.GetComponent<SC_PlayerController>().FreezeMovement(false);
+        animator.transform.parent.GetComponent<SC_PlayerController>().FreezeMovement(false);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
