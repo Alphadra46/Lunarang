746234ea3c979f4fb4f9778a446b59df7b73ff3e
Shuu <56737334+ShuuFleur@users.noreply.github.com>
2024-01-09 10:21:27 +0100

    Weaponnnnnnnnnnnnns

--- Lunarang/Assets/Scripts/Scriptable Objects/Weapons/SC_Weapon.cs
+++ Lunarang/Assets/Scripts/Scriptable Objects/Weapons/SC_Weapon.cs
@@ -9,6 +9,12 @@ public class SC_Weapon : ScriptableObject
     public string id;
     public WeaponType type;
     public ParameterType parameter;
+    public int TypeStrength;
+    public ImpactPoint impactPoint;
+    public float rayon;
+    public float areaSize;
     public List<float> MovesValues = new List<float>();
+    public float atkSpeed;
+    
     
 }
