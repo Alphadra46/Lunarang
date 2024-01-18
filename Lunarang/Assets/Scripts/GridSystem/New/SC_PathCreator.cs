bef7e3f3a006db55723e1861525de9db319de53d
Alphadra46 <degrange.kevin@gmail.com>
2024-01-18 10:29:42 +0100

    Updates on the SC_PathCreator

--- Lunarang/Assets/Scripts/GridSystem/New/SC_PathCreator.cs
+++ Lunarang/Assets/Scripts/GridSystem/New/SC_PathCreator.cs
@@ -13,6 +13,8 @@ public class SC_PathCreator : MonoBehaviour
     public int range;
     private ABPath p;
 
+    private bool isInit = false;
+
     private List<GridNode> nodesInRange = new List<GridNode>();
 
     // Start is called before the first frame update
@@ -21,79 +23,77 @@ public class SC_PathCreator : MonoBehaviour
         // Debug.Log(AstarPath.active.data.gridGraph.nodes[1].position);
 
         //Not random
-        p = ABPath.Construct((Vector3)AstarPath.active.data.gridGraph.nodes[0].position,(Vector3)AstarPath.active.data.gridGraph.nodes[15].position);
+        // p = ABPath.Construct((Vector3)AstarPath.active.data.gridGraph.nodes[0].position,(Vector3)AstarPath.active.data.gridGraph.nodes[15].position);
 
+        
         //Random
-        //p = ABPath.Construct((Vector3)AstarPath.active.data.gridGraph.nodes[Random.Range(0,AstarPath.active.data.gridGraph.nodes.Length)].position,);
+        GetNodesInRange(range);
+        //var start = (Vector3)AstarPath.active.data.gridGraph.nodes[Random.Range(0, AstarPath.active.data.gridGraph.nodes.Length)].position;
+        var start = (Vector3)AstarPath.active.data.gridGraph.nodes[startNodeIndex].position;
+        var end = (Vector3)AstarPath.active.data.gridGraph.nodes[nodesInRange[Random.Range(0,nodesInRange.Count)].NodeInGridIndex].position;
+        p = ABPath.Construct(start, end);
         
         AstarPath.StartPath(p);
+
+        isInit = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isInit)
+            return;
+        
         if (p.IsDone() && Input.GetKeyDown(KeyCode.G))
         {
-            nodesInRange.Clear();
-            // foreach (var node in p.path)
-            // {
-            //     Destroy(Instantiate(cube, (Vector3)node.position, cube.transform.rotation),5f);
-            // }
-            
-            
-            
-            for (int i = 0; i < range+1; i++)
+
+            foreach (var node in p.path)
             {
-                var Y = i * 10;
-                var X = range - i;
-                
-
-                //TODO - Check if there is already a cube on a node so that it can't spawn (using position)
-                
-                if ((startNodeIndex + Y + X<(startNodeIndex - startNodeIndex%10)+(i+1)*10) && ((startNodeIndex - startNodeIndex%10) + Y + X>=i*10))
-                {
-                    if (nodesInRange.Contains(AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y + X]))
-                        continue;
-                    
-                    nodesInRange.Add(AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y + X]);
-                    //Destroy(Instantiate(cube, (Vector3)AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y + X].position, cube.transform.rotation),3f); //Initial Point
-                }
-
-                if ((startNodeIndex + Y - X<(startNodeIndex - startNodeIndex%10)+(i+1)*10) && ((startNodeIndex - startNodeIndex%10) + Y - X>=i*10))
-                {
-                    if (nodesInRange.Contains(AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y - X]))
-                        continue;
-                    
-                    nodesInRange.Add(AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y - X]);
-                    //Destroy(Instantiate(cube, (Vector3)AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y - X].position, cube.transform.rotation),3f); //Mirror on X
-                }
-
-                if ((startNodeIndex - Y + X<(startNodeIndex - startNodeIndex%10)+(-i+1)*10) && ((startNodeIndex - startNodeIndex%10) - Y + X>=-i*10))
-                {
-                    if (nodesInRange.Contains(AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y + X]))
-                        continue;
-                    
-                    nodesInRange.Add(AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y + X]);
-                    //Destroy(Instantiate(cube, (Vector3)AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y + X].position, cube.transform.rotation),3f); //Mirror on Y
-                }
-
-                if ((startNodeIndex - Y - X<(startNodeIndex - startNodeIndex%10)+(-i+1)*10) && ((startNodeIndex - startNodeIndex%10) - Y - X>=-i*10))
-                {
-                    if (nodesInRange.Contains(AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y - X]))
-                        continue;
-
-                    nodesInRange.Add(AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y - X]);
-                    //Destroy(Instantiate(cube, (Vector3)AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y - X].position, cube.transform.rotation),3f); //Mirror on X & Y
-                }
+                Destroy(Instantiate(cube, (Vector3)node.position, cube.transform.rotation),3f);
             }
+        }
 
+        if (p.IsDone() && Input.GetKeyDown(KeyCode.R))
+        {
             foreach (var node in nodesInRange)
             {
                 Destroy(Instantiate(cube, (Vector3)node.position,cube.transform.rotation),3f);
             }
-            
         }
+    }
+    
+    public void GetNodesInRange(int searchRange)
+    {
+        nodesInRange.Clear();
+            
+        for (int i = 0; i < searchRange+1; i++)
+        {
+            var Y = i * 10;
+            var X = searchRange - i;
 
-        
+            if ((startNodeIndex + Y + X < (startNodeIndex - startNodeIndex%10)+(i+1)*10) && startNodeIndex + Y + X >= startNodeIndex - startNodeIndex%10 + i*10 && startNodeIndex + Y + X <AstarPath.active.data.gridGraph.nodes.Length) //Initial Point
+            {
+                if (!nodesInRange.Contains(AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y + X]))
+                    nodesInRange.Add(AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y + X]);
+            }
+
+            if ((startNodeIndex + Y - X < (startNodeIndex - startNodeIndex%10)+(i+1)*10) && startNodeIndex + Y - X >= startNodeIndex - startNodeIndex%10 + i*10 && startNodeIndex + Y - X <AstarPath.active.data.gridGraph.nodes.Length) //Mirror on X
+            {
+                if (!nodesInRange.Contains(AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y - X]))
+                    nodesInRange.Add(AstarPath.active.data.gridGraph.nodes[startNodeIndex + Y - X]);
+            }
+
+            if ((startNodeIndex - Y + X >= (startNodeIndex - startNodeIndex%10)-(i)*10) && startNodeIndex - Y + X < startNodeIndex - startNodeIndex%10 - (i-1)*10 && startNodeIndex - Y + X >=0) //Mirror on Y
+            {
+                if (!nodesInRange.Contains(AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y + X]))  
+                    nodesInRange.Add(AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y + X]);
+            }
+
+            if ((startNodeIndex - Y - X >= (startNodeIndex - startNodeIndex%10)-(i)*10) && startNodeIndex - Y - X < startNodeIndex - startNodeIndex%10 + (i-1)*10 && startNodeIndex - Y - X >=0) //Mirror on X & Y
+            {
+                if (!nodesInRange.Contains(AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y - X]))
+                    nodesInRange.Add(AstarPath.active.data.gridGraph.nodes[startNodeIndex - Y - X]);
+            }
+        }
     }
 }
