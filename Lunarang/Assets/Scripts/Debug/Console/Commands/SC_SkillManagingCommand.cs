fac3af58b96635f0b4ea74d2a54d714f667a3088
Shuu <56737334+ShuuFleur@users.noreply.github.com>
2024-04-03 14:43:40 +0200

    Added Skill Command

---
+++ Lunarang/Assets/Scripts/Debug/Console/Commands/SC_SkillManagingCommand.cs
@@ -0,0 +1,21 @@
+﻿public class SC_SkillManagingCommand : SC_Command
+{
+    public override string descText => "Give/Remove skills.";
+    
+    public override void Execute(string[] args)
+    {
+        switch (args.Length)
+        {
+            
+            case <= 1:
+                SC_DebugConsole.instance.PrintLine("<color=red> Please enter add/remove.");
+                break;
+            case > 2:
+                
+                SC_DebugConsole.instance.PrintLine("<color=red> Please enter add/remove.");
+                
+                break;
+            
+        }
+    }
+}
