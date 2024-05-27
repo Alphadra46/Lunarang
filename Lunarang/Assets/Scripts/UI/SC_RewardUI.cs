e3bec46c92206d20358adddc9d487612fc822d55
Shuu <56737334+ShuuFleur@users.noreply.github.com>
2024-05-27 10:44:23 +0200

    Updated Animations for RewardUI Updated HUD

--- Lunarang/Assets/Scripts/UI/SC_RewardUI.cs
+++ Lunarang/Assets/Scripts/UI/SC_RewardUI.cs
@@ -1,11 +1,10 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using Random = UnityEngine.Random;
 
 public class SC_RewardUI : MonoBehaviour
 {
@@ -16,28 +15,45 @@ public class SC_RewardUI : MonoBehaviour
     public GameObject rewardItemPrefab;
 
     public Transform rewardParent;
+    public Transform rewardStart;
     
-    private List<GameObject> rewards = new List<GameObject>();
-    
+    [ShowInInspector, ReadOnly] private List<GameObject> rewards = new List<GameObject>();
 
+
+    public void Show()
+    {
+        
+        if(!TryGetComponent(out Animator _animator)) return;
+        
+        _animator.SetTrigger("Show");
+        
+    }
+    
     public void Init()
     {
+        var index = 0;
+        
         if (rewardSkills.Count > 0)
         {
+            
             foreach (var skill in rewardSkills)
             {
                 var reward = Instantiate(rewardItemPrefab, rewardParent).GetComponent<SC_RewardItemUI>();
+                reward.gameObject.gameObject.name = "Reward_" + index;
                 reward.Init(skill);
                 
                 rewards.Add(reward.gameObject);
+                index++;
             }
-            
+
         }
+        
         if (rewardRessources.Count > 0)
         {
             foreach (var ressource in rewardRessources)
             {
                 var reward = Instantiate(rewardItemPrefab, rewardParent).GetComponent<SC_RewardItemUI>();
+                reward.gameObject.gameObject.name = "Reward_" + index;
                 reward.Init(ressource, 1);
                 
                 rewards.Add(reward.gameObject);
@@ -53,11 +69,46 @@ public class SC_RewardUI : MonoBehaviour
             
             navigation.mode = Navigation.Mode.Automatic;
 
+            reward.GetComponent<RectTransform>().anchoredPosition =
+                rewardStart.GetComponent<RectTransform>().anchoredPosition;
         }
+
+        StartCoroutine(DelayBeforeCards(1f));
         
-        EventSystem.current.SetSelectedGameObject(rewards[0]);
     }
 
+    public void ShowAnimation()
+    {
+        var lastX = 0f;
+        
+        for (var i = 0; i < rewards.Count; i++)
+        {
+        
+            var pos = rewards[i].GetComponent<RectTransform>().anchoredPosition;
+            print(pos);
+        
+            pos.x = (i > 0 ? lastX : 225) + (i > 0 ? 12.5f : 0) + (i > 0 ? 450 : 0);
+            lastX = pos.x;
+            
+            pos.y = 350;
+        
+            rewards[i].GetComponent<RectTransform>().DOAnchorPos(pos, (0.25f * (i+1))).SetUpdate(true);
+            print(pos);
+        
+        }
+
+        EventSystem.current.SetSelectedGameObject(rewards[0]);
+
+    }
+
+    public IEnumerator DelayBeforeCards(float delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        
+        ShowAnimation();
+    }
+    
+    
     private void OnDestroy()
     {
         foreach (var reward in rewards)
